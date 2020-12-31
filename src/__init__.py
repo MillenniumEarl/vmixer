1a78f2cd3fc301ab3afad1a3ab1fe75cb621978b
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2020-12-31 10:49:28 +0100

    Add video_hash_similarity function

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,33 @@
 # Core modules
 import shutil
 import tempfile
-from typing import List
+from typing import List, Tuple
 
 # Project modules
-from .video_utility import sync_video, whash_video
+from .video_utility import sync_video, whash_video, compare_video_hash
 from .scene_parser import extract_scenes, compare_scenes, sync_scenes
 
+# Aliases and types
+FrameHash = Tuple[float, str]
 
 def video_similarity(reference_path:str, comparative_path:str, threshold=6.0) -> float:
+    """Compare two videos to find similarities. Returns a value 
+        between 0 and 1, where 0 indicates no similarity and 1 returns equality
+
+        The first video passed by parameter is taken as a reference, 
+        inverting the videos could change the result
+    
+    Args:
+        reference_path (str): Path to reference video
+        comparative_path (str): Path of the video to compare
+        threshold (float, optional): Threshold used to detect scene change. 
+                                    The smaller it is, the more scenes are detected. 
+                                    Defaults to 6.0.
+
+    Returns:
+        float: Similarity of videos, value between 0 and 1
+    """
+    
     # Local variables
     tmp_ref_dest = tempfile.mkdtemp()
     tmp_cmp_dest = tempfile.mkdtemp()
@@ -27,7 +46,32 @@ def video_similarity(reference_path:str, comparative_path:str, threshold=6.0) ->
     return similarity
 
 
+def video_hash_similarity(reference_hash_list: List[FrameHash], compare_hash_list: List[FrameHash]) -> float:
+    """It compares the perceptual hashes of two videos 
+        and returns a similarity index between 0 and 1
+
+    Args:
+        reference_hash_list (List[FrameHash]): Hash of the reference video
+        compare_hash_list (List[FrameHash]): Hash of the video to compare
+
+    Returns:
+        float: Video similarity index. Between 0 and 1
+    """
+    return compare_video_hash(reference_hash_list, compare_hash_list)
+
+
 def video_merge(reference_path: str, comparative_path: str, dest: str, threshold=6.0):
+    """Compare two videos and synchronize them on common frames, 
+        creating a single synchronized video
+
+    Args:
+        reference_path (str): Path to reference video
+        comparative_path (str): Path of the video to compare
+        dest (str): Destination of the merged video
+        threshold (float, optional): Threshold used to detect scene change. 
+                                    The smaller it is, the more scenes are detected. 
+                                    Defaults to 6.0.
+    """
     # Local variables
     tmp_ref_dest = tempfile.mkdtemp()
     tmp_cmp_dest = tempfile.mkdtemp()
@@ -45,6 +89,14 @@ def video_merge(reference_path: str, comparative_path: str, dest: str, threshold
 
 
 def video_hash(filename:str) -> List[str]:
+    """Calculate the perceptual hashes of a video
+
+    Args:
+        filename (str): Path of the video
+
+    Returns:
+        List[str]: List of hashes that identify the video
+    """
     # Hash video
     result = whash_video(filename)
 
