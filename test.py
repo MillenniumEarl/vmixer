67326618d1dfed07a26c5ab8f334805dc7b42b75
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-01-01 22:11:22 +0100

    Reorganized file

--- test.py
+++ test.py
@@ -6,16 +6,25 @@ from src.__init__ import video_similarity, video_merge
 from src.scene_parser import find_optimal_threshold
 
 # Constants
-REFERENCE_FILE = os.path.join('test', 'C', 'cut1.mp4')
-COMPARE_FILE = os.path.join('test', 'C', 'cut2.mp4')
+ORIGINAL_FILE = os.path.join('test', 'A', 'original.mp4')
+REFERENCE_FILE = os.path.join('test', 'A', 'cut1.mp4')
+COMPARE_FILE = os.path.join('test', 'A', 'cut2.mp4')
 MERGED_FILE = os.path.join('test', 'result.mp4')
-THRESHOLD = find_optimal_threshold(REFERENCE_FILE)
+
+# Find optimal threshold for reference file
+threshold = find_optimal_threshold(REFERENCE_FILE, sec_for_scene=0.5)
+print(f'Optimal threshold is: {threshold:.2f}')
 
 # Find similarity between videos
-similarity = video_similarity(REFERENCE_FILE, COMPARE_FILE, THRESHOLD)
-print(f'Video similarity: {similarity*100:.2f}%')
+similarity = video_similarity(REFERENCE_FILE, COMPARE_FILE, threshold)
+print(f'Video similarity: {similarity * 100:.2f}%')
 
 # Merge videos
 if similarity > 0:
-    video_merge(REFERENCE_FILE, COMPARE_FILE, MERGED_FILE, THRESHOLD)
+    video_merge(REFERENCE_FILE, COMPARE_FILE, MERGED_FILE, threshold)
     print('Video merged')
+    
+# Check similarity with original file
+if os.path.exists(ORIGINAL_FILE):
+    similarity = video_similarity(ORIGINAL_FILE, MERGED_FILE, threshold)
+    print(f'Video similarity from original: {similarity * 100:.2f}%')
