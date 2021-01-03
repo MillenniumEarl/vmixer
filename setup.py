a5804b27553e8c70ea4594d5c0310a09c583b253
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-01-03 10:35:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vmixer",
-    version="1.0.0",
+    version="0.0.1",
     author="MillenniumEarl",
     description="POC for merging similar videos",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MillenniumEarl/vmixer",
     packages=setuptools.find_packages(),
-    package_dir={'': 'src'},
+    install_requires=[],
     license="MIT",
     classifiers=[
         'Development Status :: 3 - Alpha',
