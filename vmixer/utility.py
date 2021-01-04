94495caeb4c1e54584da8ea18da6118969aca29a
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-01-04 17:49:14 +0100

    Remove duplicates entry

--- vmixer/utility.py
+++ vmixer/utility.py
@@ -64,9 +64,9 @@ def videohash_similarity(ref_hash_list: List[FrameHash], cmp_hash_list: List[Fra
         float: Similarity index between 0 and 1
     """
 
-    # Local variables
-    cmp_hashes = [item[1] for item in cmp_hash_list]
-    ref_hashes = [item[1] for item in ref_hash_list]
+    # Local variables (remove duplicates as will slow down the process)
+    cmp_hashes = list(set([item[1] for item in cmp_hash_list]))
+    ref_hashes = list(set([item[1] for item in ref_hash_list]))
 
     # Count the hash matches
     comparison = [hash for hash in cmp_hashes if hash in ref_hashes]
